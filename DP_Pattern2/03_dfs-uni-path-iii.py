f83974fb2831c2d6713a81e669b50fc379c701fa
Tigger <minhthe.007@gmail.com>
2020-02-05 03:40:39 +0000

    enhance the code- return directly result

--- DP_Pattern2/03_dfs-uni-path-iii.py
+++ DP_Pattern2/03_dfs-uni-path-iii.py
@@ -2,46 +2,33 @@
 https://leetcode.com/problems/unique-paths-iii/
 
 '''
-def dfs(visited, grid, pos_start, pos_end, cnt_condition, cnt_obstacle, rst,dx, dy, m ,n):
-	if(cnt_condition == -1 and pos_start[0] == pos_end[0] and pos_start[1] == pos_end[1]):
-		rst.append(1)
-		return 
-	
+def	dfs(grid, visited, rst, start , end, cnt_condition, m , n, dx, dy):
+	cnt = 0 
+	if(cnt_condition == -1 and start[0] == end[0] and start[1] == end[1]):
+		return 1 
 	for i in range(4):
-		x, y = dx[i] + pos_start[0], dy[i] + pos_start[1]
-		if(x in range(m) and y in range(n) and visited[x][y] == False):
-			if( grid[x][y] == 0 or (grid[x][y] == 2 and cnt_condition == 0) ):
-				visited[x][y] = True
-				dfs(visited, grid, (x,y), pos_end, cnt_condition-1, cnt_obstacle, rst,dx, dy, m ,n)
-				visited[x][y] = False
-				# cnt_condition +=1
-						 
-				 
-				
+		x , y = dx[i] + start[0], dy[i] + start[1]
+		if(x in range(m) and y in range(n) and grid[x][y]!= -1 and visited[x][y] == False):
+			visited[x][y] = True
+			cnt += dfs(grid, visited, rst, (x, y) , end, cnt_condition -1 , m , n, dx, dy)
+			visited[x][y] = False
+	return cnt
+			
 class Solution(object):
 	def uniquePathsIII(self, grid):
-		pos_start, pos_end, cnt_condition, cnt_obstacle = (-1,-1),(-1,-1),0, 0
 		m = len(grid)
-		if m ==0 : return 0
+		if m == 0 : return 0
 		n = len(grid[0])
-		if n == 0: return 0
+		if n == 0 : return 0 
+		cnt_condition, start, end = 0 , (-1, -1), (-1, -1)
 		for i in range(m):
 			for j in range(n):
-				if(grid[i][j]== 0):
-					cnt_condition +=1
-				elif(grid[i][j] == 1):
-					pos_start = (i,j)
-				elif(grid[i][j] == 2):
-					pos_end = (i,j)
-				else:
-					cnt_obstacle += 1 
-		if( pos_start[0]==-1  or pos_end[0]==-1  ):
-			return 0 
-		
-		visited = [[False for i in range(n)] for j in range(m)]		
-		rst= []
-		dx, dy = [0,0,-1, 1],[-1,1,0,0]
-		visited[pos_start[0]][pos_start[1]] = True
-		dfs(visited, grid, pos_start, pos_end, cnt_condition, cnt_obstacle, rst,dx,dy,m ,n)
-		print(rst)
-		return len(rst)
+				if(grid[i][j] == 0): cnt_condition += 1 
+				elif(grid[i][j] == 1): start = (i, j)
+				elif(grid[i][j] == 2): end = (i,j)
+		if(start[0] == -1 or end[0] == -1): return 0
+		rst = []
+		visited = [[False for i in range(n)] for j in range(m)]
+		dx, dy  = [-1, 1, 0 , 0], [0 ,0 , -1, 1]
+		visited[start[0]][start[1]] = True
+		return dfs(grid, visited, rst, start , end, cnt_condition, m , n, dx, dy)	
