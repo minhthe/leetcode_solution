1ebd21c2d8ee31689859acd32c521fb53fe3716a
Tigger <minhthe.007@gmail.com>
2020-02-27 12:03:52 +0000

    one - zero - the same with knapsack

--- Easy/happyNumber1.py
+++ Easy/happyNumber1.py
@@ -4,6 +4,24 @@ https://leetcode.com/problems/happy-number/
 approach:  hash + recursion
 
 
+*** learn more:
+
+1) not using the power 2
+https://leetcode.com/problems/happy-number/discuss/445373/Python-Dictionary-and-Set-beat-99-time-and-100-space
+
+def isHappy(self, n: int) -> bool:
+        dict = {'0':0,'1':1,'2':4,'3':9,'4':16,'5':25,'6':36,'7':49,'8':64,'9':81}
+        sum_set = set()
+        res = sum([dict[x] for x in str(n)])
+        if res == 1: return True
+        while res != 1:
+            if res in sum_set:
+                return False
+            else:
+                sum_set.add(res)
+                res = sum([dict[x] for x in str(res)])
+                if res == 1: return True
+
 '''
 
 class Solution(object):
